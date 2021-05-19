6ebb8056f21a02a431713ddbc496ab2151eb54c2
Jai Jalah Kumar <jay.jk@hotmail.com>
2021-05-19 11:51:42 +0100

    Add styling with Bootstrap

--- generator/views.py
+++ generator/views.py
@@ -9,10 +9,6 @@ def home(request):
     return render(request, "generator/home.html")
 
 
-def about(request):
-    return render(request, "generator/about.html")
-
-
 def password(request):
 
     characters = list("abcdefghijklmnopqrstuvwxyz")
