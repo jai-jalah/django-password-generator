a922a1f0a25aecef86521ca2b65e7d35cd658673
Jai Jalah Kumar <jay.jk@hotmail.com>
2021-05-19 11:24:47 +0100

    Generate basic random password

--- generator/views.py
+++ generator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 
@@ -10,6 +11,13 @@ def home(request):
 
 def password(request):
 
-    thepassword = "testing"
+    characters = list("abcdefghijklmnopqrstuvwxyz")
+
+    length = 10
+
+    thepassword = ""
+
+    for x in range(length):
+        thepassword += random.choice(characters)
 
     return render(request, "generator/password.html", {"password": thepassword})
